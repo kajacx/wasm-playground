4121245d4dff47543fd975c4a0dcb7d970f38fa6
kajacx <kajacx@gmail.com>
2023-05-15 13:48:39 +0200

    Pass string back and forth correcltly

--- wasmer-plugin/src/lib.rs
+++ wasmer-plugin/src/lib.rs
@@ -1,47 +1,103 @@
+use std::{alloc::*, sync::Mutex};
+
+static TEXT: Mutex<String> = Mutex::new(String::new());
+
 #[no_mangle]
-pub fn add_three_i32(val: i32) -> i32 {
-    (unsafe { add_one_i32(val + 1) }) + 1
+pub fn push_string(text: u64) -> u64 {
+    // text
+    let bytes = import_from_host(text);
+    let text = String::from_utf8(bytes).unwrap();
+
+    append_string(&mut *TEXT.lock().unwrap(), text, transmute_via_host);
+
+    let cloned = TEXT
+        .lock()
+        .unwrap()
+        .as_bytes()
+        .to_owned()
+        .into_boxed_slice();
+    export_to_host(cloned)
+}
+
+#[no_mangle]
+pub fn remove_chars(num: u32) {
+    shrink_string(&mut *TEXT.lock().unwrap(), num)
+}
+
+fn export_to_host(mut data: Box<[u8]>) -> u64 {
+    let len = data.len();
+    let addr = data.as_mut_ptr() as usize;
+    std::mem::forget(data);
+    to_fatptr(addr, len)
+}
+
+fn import_from_host(fatptr: u64) -> Vec<u8> {
+    let (addr, len) = from_fatptr(fatptr);
+    // SAFETY: Host is giving us full ownership of these bytes
+    unsafe { Vec::from_raw_parts(addr as *mut u8, len, len) }
+}
+
+fn transmute_via_host(input: String) -> String {
+    let fatptr = export_to_host(input.into_bytes().into_boxed_slice());
+    let transmuted = unsafe { transform_string(fatptr) };
+    let bytes = import_from_host(transmuted);
+    String::from_utf8(bytes).unwrap()
 }
 
 #[link(wasm_import_module = "my_imports")]
 extern "C" {
-    fn add_one_i32(arg: i32) -> i32;
+    fn transform_string(string: u64) -> u64;
 }
 
 #[no_mangle]
-pub fn add_three_f32(left: f32) -> f32 {
-    (unsafe { add_one_f32(left + 1.0) }) + 1.0
-}
-
-#[link(wasm_import_module = "my_imports")]
-extern "C" {
-    fn add_one_f32(arg: f32) -> f32;
-}
-
-#[no_mangle]
-pub fn exported_returns_bool() -> bool {
-    unsafe { imported_returns_bool() }
-}
-
-#[no_mangle]
-pub fn exported_returns_u8() -> u8 {
-    unsafe { imported_returns_u8() }
+pub fn allocate_for_host(size: usize) -> u64 {
+    // let values = Box::new([0u8; 20]);
+    // let x = Box::leak(values);
+    // return x as *mut _ as usize as u64;
+    let addr = if size == 0 {
+        0
+    } else {
+        let layout = Layout::from_size_align(size, 1).unwrap();
+        // SAFETY: size is not zero
+        let bytes = unsafe { alloc(layout) };
+        bytes as *mut u8 as usize
+    };
+    to_fatptr(addr, size)
 }
 
 #[no_mangle]
-pub fn exported_takes_bool(arg: bool) {
-    unsafe { imported_takes_bool(arg) }
+pub fn free_from_host(fatptr: u64) {
+    let (addr, len) = from_fatptr(fatptr);
+    if len != 0 {
+        let layout = Layout::from_size_align(len, 1).unwrap();
+        // SAFETY: size is not zero, and host guarantees to not use these bytes anymore
+        unsafe { dealloc(addr as *mut u8, layout) };
+    };
 }
 
-#[no_mangle]
-pub fn exported_takes_u8(arg: u8) {
-    unsafe { imported_takes_u8(arg) }
+// Common between host and plugin:
+
+fn append_string(
+    changed_string: &mut String,
+    appended_string: String,
+    mut transmutor: impl FnMut(String) -> String,
+) {
+    let appended_string = appended_string + " appended";
+    let appended_string = transmutor(appended_string);
+    changed_string.push_str(&appended_string);
 }
 
-#[link(wasm_import_module = "my_imports")]
-extern "C" {
-    fn imported_returns_bool() -> bool;
-    fn imported_returns_u8() -> u8;
-    fn imported_takes_bool(arg: bool);
-    fn imported_takes_u8(arg: u8);
+fn shrink_string(changed_string: &mut String, byte_count: u32) {
+    let len = changed_string.len();
+    changed_string.replace_range((len - byte_count as usize)..len, "");
+}
+
+fn from_fatptr(fatptr: u64) -> (usize, usize) {
+    let addr = fatptr as u32 as usize;
+    let len = (fatptr >> 32) as usize;
+    (addr, len)
+}
+
+fn to_fatptr(addr: usize, len: usize) -> u64 {
+    (addr as u32) as u64 | (len as u64) << 32
 }
--- wasmer3-runtime/src/main.rs
+++ wasmer3-runtime/src/main.rs
@@ -1,10 +1,13 @@
 use wasmer::*;
 
-struct MyEnv;
+struct Env {
+    instance: Option<&'static Instance>,
+    store: usize,
+}
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let wasm_bytes = include_bytes!(
-        "../../wasmer-plugin/target/wasm32-unknown-unknown/debug/wasmer2_plugin.wasm"
+        "../../wasmer-plugin/target/wasm32-unknown-unknown/debug/wasmer_plugin.wasm"
     )
     .as_ref();
 
@@ -15,23 +18,32 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Let's compile the Wasm module.
     let module = Module::new(&store, wasm_bytes)?;
 
-    let env = FunctionEnv::new(&mut store, MyEnv {});
-
-    fn add_one_i32(_env: FunctionEnvMut<MyEnv>, a: i32) -> i32 {
-        a + 1
-    }
-    let add_one_i32_native = Function::new_typed_with_env(&mut store, &env, add_one_i32);
-
-    fn add_one_f32(a: f32) -> f32 {
-        a + 1.0
-    }
-    let add_one_f32_native = Function::new_typed(&mut store, add_one_f32);
+    let env = FunctionEnv::new(
+        &mut store,
+        Env {
+            instance: None,
+            store: 0,
+        },
+    );
 
     // Create an empty import object.
     let import_object = imports! {
         "my_imports" => {
-            "add_one_i32" => add_one_i32_native,
-            "add_one_f32" => add_one_f32_native,
+            "transform_string" => Function::new_typed_with_env(&mut store, &env, |envf: FunctionEnvMut<Env>, input: u64| {
+                let store =unsafe {
+                    &mut * (envf.data().store as *mut Store)
+                };
+                let instance = envf.data().instance.unwrap();
+                let memory = instance.exports.get_memory("memory").unwrap();
+
+                let bytes = import_from_plugin(instance, memory, store, input);
+                let text = String::from_utf8(bytes).unwrap();
+
+                let transmuted = transmute_string(text);
+
+                let exported = export_to_plugin(&memory,  store, instance, &transmuted.into_bytes());
+                exported
+            })
         }
     };
 
@@ -39,24 +51,132 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Let's instantiate the Wasm module.
     let instance = Instance::new(&mut store, &module, &import_object)?;
 
-    let add_three_i32: TypedFunction<i32, i32> = instance
-        .exports
-        .get_function("add_three_i32")?
-        .typed(&mut store)?;
+    let memory = instance.exports.get_memory("memory").unwrap();
 
-    println!("Calling `add_three_i32` function...");
-    let results = add_three_i32.call(&mut store, 5)?;
+    let store_address = &store as *const _ as usize;
+    let mut env_mut = env.as_mut(&mut store);
+    env_mut.store = store_address;
+    unsafe {
+        env_mut.instance = Some(&*(&instance as *const _));
+    }
 
-    println!("Results: {:?}", results);
-    assert_eq!(results, 8);
+    let mut compare_string = String::new();
 
-    let add_three_f32 = instance.exports.get_function("add_three_f32")?;
+    for i in 0..10_000_000u32 {
+        if i % 10_000 == 0 {
+            println!("{}: {}", i, compare_string.len());
+        }
 
-    println!("Calling `add_three_f32` function...");
-    let results = add_three_f32.call(&mut store, &[Value::F32(5.5)])?;
-
-    println!("Results: {:?}", results);
-    assert_eq!(results.to_vec(), vec![Value::F32(8.5)]);
+        if compare_string.len() < 1000 {
+            grow_strings(memory, &mut store, &instance, &mut compare_string, i);
+            continue;
+        }
+        if compare_string.len() > 1000000 {
+            shrink_strings(&mut store, &instance, &mut compare_string, i % 100);
+            continue;
+        }
+        if i % 13 < 5 {
+            grow_strings(memory, &mut store, &instance, &mut compare_string, i);
+        } else {
+            shrink_strings(&mut store, &instance, &mut compare_string, i % 100);
+        }
+    }
 
     Ok(())
+}
+
+fn grow_strings(
+    memory: &Memory,
+    store: &mut Store,
+    instance: &Instance,
+    compare_string: &mut String,
+    n: u32,
+) {
+    let appendings = format!("Growing: {n}, ");
+    append_string(compare_string, appendings.clone(), transmute_string);
+
+    let exported = export_to_plugin(memory, store, instance, appendings.as_bytes());
+
+    let push_str = instance
+        .exports
+        .get_typed_function::<u64, u64>(&store, "push_string")
+        .unwrap();
+    let ret = push_str.call(store, exported).unwrap();
+
+    let imported = import_from_plugin(instance, memory, store, ret);
+    let check = String::from_utf8(imported).unwrap();
+
+    let a: &str = compare_string.as_str();
+    let b: &str = check.as_str();
+    assert_eq!(a, b);
+}
+
+fn shrink_strings(store: &mut Store, instance: &Instance, compare_string: &mut String, n: u32) {
+    shrink_string(compare_string, n);
+    let rm_chars = instance
+        .exports
+        .get_typed_function::<u32, ()>(&store, "remove_chars")
+        .unwrap();
+    rm_chars.call(store, n).unwrap();
+}
+
+fn import_from_plugin(
+    instace: &Instance,
+    memory: &Memory,
+    store: &mut Store,
+    fatptr: u64,
+) -> Vec<u8> {
+    let (addr, len) = from_fatptr(fatptr);
+    let mut bytes = vec![0; len];
+    let view = memory.view(store);
+    view.read(addr as u64, &mut bytes[0..len]).unwrap();
+
+    let free = instace
+        .exports
+        .get_typed_function::<u64, ()>(store, "free_from_host")
+        .unwrap();
+    free.call(store, fatptr).unwrap();
+
+    bytes
+}
+
+fn export_to_plugin(memory: &Memory, store: &mut Store, instance: &Instance, data: &[u8]) -> u64 {
+    let allocate = instance
+        .exports
+        .get_typed_function::<u32, u64>(&store, "allocate_for_host")
+        .unwrap();
+    let mut allocate = |size: u32| allocate.call(store, size).unwrap();
+
+    let fatptr = allocate(data.len() as u32);
+    let (addr, _) = from_fatptr(fatptr);
+    let view = memory.view(store);
+    view.write(addr as u64, data).unwrap();
+    fatptr
+}
+
+fn transmute_string(text: String) -> String {
+    text + " transmuted in host"
+}
+
+// Common between host and plugin:
+
+fn append_string(
+    changed_string: &mut String,
+    appended_string: String,
+    mut transmutor: impl FnMut(String) -> String,
+) {
+    let appended_string = appended_string + " appended";
+    let appended_string = transmutor(appended_string);
+    changed_string.push_str(&appended_string);
+}
+
+fn shrink_string(changed_string: &mut String, byte_count: u32) {
+    let len = changed_string.len();
+    changed_string.replace_range((len - byte_count as usize)..len, "");
+}
+
+fn from_fatptr(fatptr: u64) -> (usize, usize) {
+    let addr = fatptr as u32 as usize;
+    let len = (fatptr >> 32) as usize;
+    (addr, len)
 }
