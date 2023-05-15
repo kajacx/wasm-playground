8e41867ca4784cd2f7930a9985bfc9464c6b1122
kajacx <kajacx@gmail.com>
2023-05-15 19:02:54 +0200

    Keep only add three, still error

--- plugin/src/lib.rs
+++ plugin/src/lib.rs
@@ -1,28 +1,9 @@
 wai_bindgen_rust::export!("../protocol-plugin.wai");
 
-use protocol_plugin::*;
 struct ProtocolPlugin;
 
 impl protocol_plugin::ProtocolPlugin for ProtocolPlugin {
     fn add_three(number: u32) -> u32 {
         number + 3
-    }
-
-    fn get_color() -> Color {
-        Color {
-            r: 0.2,
-            g: 1.0,
-            b: 0.4,
-        }
-    }
-
-    fn get_complex() -> Complex {
-        Complex {
-            name: "Hello".into(),
-            colors: vec![Self::get_color()],
-            // my: MyEnum::One,
-            my: vec![],
-            mys: vec![],
-        }
     }
 }
