2c9c4594ac5b04bcc4df709493f9d012eec0907d
kajacx <kajacx@gmail.com>
2023-05-16 18:01:52 +0200

    Fix pkg gitignore

--- run-wasm-only-forked.sh
+++ run-wasm-only-forked.sh
@@ -4,6 +4,7 @@ cd plugin && cargo build --release --target wasm32-unknown-unknown && cd ..  && 
 cd wasm-only-forked && \
 wasm-pack build --target web && \
 # wasm-bindgen --out-dir ./out/ --target web ./target/wasm32-unknown-unknown/debug/bevy-web-test.wasm && \
+cp pkg.gitignore pkg/.gitignore
 cd pkg && docker-compose -p forked down && docker-compose -p forked up -d && cd .. && \
 cd .. && \
 echo "Done, view at http://localhost:8092"
