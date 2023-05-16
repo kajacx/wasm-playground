a375dec0fc3bfde66f77c7c81662b8caed86ec17
kajacx <kajacx@gmail.com>
2023-05-16 18:15:02 +0200

    Hang on ...
    Update code

--- run-no-wai-web.sh
+++ run-no-wai-web.sh
@@ -4,7 +4,7 @@ cd plugin && cargo build --release --target wasm32-unknown-unknown && cd ..  && 
 cd no-wai-web && \
 wasm-pack build --target web && \
 # wasm-bindgen --out-dir ./out/ --target web ./target/wasm32-unknown-unknown/debug/bevy-web-test.wasm && \
-cp pkg.gitignore pkg/.gitignore
-cd pkg && docker-compose -p forked down && docker-compose -p forked up -d && cd .. && \
+cp pkg.gitignore pkg/.gitignore && \
+cd pkg && docker-compose -p no-wai-web down && docker-compose -p no-wai-web up -d && cd .. && \
 cd .. && \
 echo "Done, view at http://localhost:8093"
