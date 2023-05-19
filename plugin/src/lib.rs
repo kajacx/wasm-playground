e22542b1af3a6e4f2649d531bcee8e5211b68fb2
kajacx <kajacx@gmail.com>
2023-05-19 13:25:51 +0200

    Add a complex type (runtime error)

--- plugin/src/lib.rs
+++ plugin/src/lib.rs
@@ -6,6 +6,14 @@ impl protocol_plugin::ProtocolPlugin for ProtocolPlugin {
     fn add_three(number: u32) -> u32 {
         number + 3
     }
+
+    fn get_color() -> protocol_plugin::Color {
+        protocol_plugin::Color {
+            r: 0.1,
+            g: 0.5,
+            b: 0.0,
+        }
+    }
 }
 
 // #[no_mangle]
--- wasm-only-forked/src/lib.rs
+++ wasm-only-forked/src/lib.rs
@@ -9,98 +9,98 @@ use wasm_bindgen::prelude::*;
 const PLUGIN_BYTES: &'static [u8] =
     include_bytes!("../../plugin/target/wasm32-unknown-unknown/release/bevy_plugin.wasm");
 
-// wai_bindgen_wasmer::import!("../protocol-plugin.wai");
+wai_bindgen_wasmer::import!("../protocol-plugin.wai");
 
-#[allow(clippy::all)]
-pub mod protocol_plugin {
-    #[allow(unused_imports)]
-    use wai_bindgen_wasmer::{anyhow, wasmer};
-    /// Auxiliary data associated with the wasm exports.
-    pub struct ProtocolPluginData {}
-    #[automatically_derived]
-    impl ::core::default::Default for ProtocolPluginData {
-        #[inline]
-        fn default() -> ProtocolPluginData {
-            ProtocolPluginData {}
-        }
-    }
-    pub struct ProtocolPlugin {
-        #[allow(dead_code)]
-        env: wasmer::FunctionEnv<ProtocolPluginData>,
-        func_add_three: wasmer::TypedFunction<i32, i32>,
-    }
-    impl ProtocolPlugin {
-        #[allow(unused_variables)]
-        /// Adds any intrinsics, if necessary for this exported wasm
-        /// functionality to the `ImportObject` provided.
-        ///
-        /// This function returns the `ProtocolPluginData` which needs to be
-        /// passed through to `ProtocolPlugin::new`.
-        fn add_to_imports(
-            mut store: impl wasmer::AsStoreMut,
-            imports: &mut wasmer::Imports,
-        ) -> wasmer::FunctionEnv<ProtocolPluginData> {
-            let env = wasmer::FunctionEnv::new(&mut store, ProtocolPluginData::default());
-            env
-        }
-        /// Instantiates the provided `module` using the specified
-        /// parameters, wrapping up the result in a structure that
-        /// translates between wasm and the host.
-        ///
-        /// The `imports` provided will have intrinsics added to it
-        /// automatically, so it's not necessary to call
-        /// `add_to_imports` beforehand. This function will
-        /// instantiate the `module` otherwise using `imports`, and
-        /// both an instance of this structure and the underlying
-        /// `wasmer::Instance` will be returned.
-        pub fn instantiate(
-            mut store: impl wasmer::AsStoreMut,
-            module: &wasmer::Module,
-            imports: &mut wasmer::Imports,
-        ) -> anyhow::Result<(Self, wasmer::Instance)> {
-            let env = Self::add_to_imports(&mut store, imports);
-            let instance = wasmer::Instance::new(&mut store, module, &*imports)?;
-            Ok((Self::new(store, &instance, env)?, instance))
-        }
-        /// Low-level creation wrapper for wrapping up the exports
-        /// of the `instance` provided in this structure of wasm
-        /// exports.
-        ///
-        /// This function will extract exports from the `instance`
-        /// and wrap them all up in the returned structure which can
-        /// be used to interact with the wasm module.
-        pub fn new(
-            store: impl wasmer::AsStoreMut,
-            _instance: &wasmer::Instance,
-            env: wasmer::FunctionEnv<ProtocolPluginData>,
-        ) -> Result<Self, wasmer::ExportError> {
-            let func_add_three = _instance
-                .exports
-                .get_function("add-three")
-                .expect("GET IT")
-                .typed(&store)
-                .unwrap_or_else(|e| panic!("{e:?}"));
-            Ok(ProtocolPlugin {
-                func_add_three,
-                env,
-            })
-        }
-        pub fn add_three(
-            &self,
-            store: &mut wasmer::Store,
-            number: u32,
-        ) -> Result<u32, wasmer::RuntimeError> {
-            let result0 = self
-                .func_add_three
-                .call(store, wai_bindgen_wasmer::rt::as_i32(number))?;
-            Ok(result0 as u32)
-        }
-    }
-    #[allow(unused_imports)]
-    use wasmer::AsStoreMut as _;
-    #[allow(unused_imports)]
-    use wasmer::AsStoreRef as _;
-}
+// #[allow(clippy::all)]
+// pub mod protocol_plugin {
+//     #[allow(unused_imports)]
+//     use wai_bindgen_wasmer::{anyhow, wasmer};
+//     /// Auxiliary data associated with the wasm exports.
+//     pub struct ProtocolPluginData {}
+//     #[automatically_derived]
+//     impl ::core::default::Default for ProtocolPluginData {
+//         #[inline]
+//         fn default() -> ProtocolPluginData {
+//             ProtocolPluginData {}
+//         }
+//     }
+//     pub struct ProtocolPlugin {
+//         #[allow(dead_code)]
+//         env: wasmer::FunctionEnv<ProtocolPluginData>,
+//         func_add_three: wasmer::TypedFunction<i32, i32>,
+//     }
+//     impl ProtocolPlugin {
+//         #[allow(unused_variables)]
+//         /// Adds any intrinsics, if necessary for this exported wasm
+//         /// functionality to the `ImportObject` provided.
+//         ///
+//         /// This function returns the `ProtocolPluginData` which needs to be
+//         /// passed through to `ProtocolPlugin::new`.
+//         fn add_to_imports(
+//             mut store: impl wasmer::AsStoreMut,
+//             imports: &mut wasmer::Imports,
+//         ) -> wasmer::FunctionEnv<ProtocolPluginData> {
+//             let env = wasmer::FunctionEnv::new(&mut store, ProtocolPluginData::default());
+//             env
+//         }
+//         /// Instantiates the provided `module` using the specified
+//         /// parameters, wrapping up the result in a structure that
+//         /// translates between wasm and the host.
+//         ///
+//         /// The `imports` provided will have intrinsics added to it
+//         /// automatically, so it's not necessary to call
+//         /// `add_to_imports` beforehand. This function will
+//         /// instantiate the `module` otherwise using `imports`, and
+//         /// both an instance of this structure and the underlying
+//         /// `wasmer::Instance` will be returned.
+//         pub fn instantiate(
+//             mut store: impl wasmer::AsStoreMut,
+//             module: &wasmer::Module,
+//             imports: &mut wasmer::Imports,
+//         ) -> anyhow::Result<(Self, wasmer::Instance)> {
+//             let env = Self::add_to_imports(&mut store, imports);
+//             let instance = wasmer::Instance::new(&mut store, module, &*imports)?;
+//             Ok((Self::new(store, &instance, env)?, instance))
+//         }
+//         /// Low-level creation wrapper for wrapping up the exports
+//         /// of the `instance` provided in this structure of wasm
+//         /// exports.
+//         ///
+//         /// This function will extract exports from the `instance`
+//         /// and wrap them all up in the returned structure which can
+//         /// be used to interact with the wasm module.
+//         pub fn new(
+//             store: impl wasmer::AsStoreMut,
+//             _instance: &wasmer::Instance,
+//             env: wasmer::FunctionEnv<ProtocolPluginData>,
+//         ) -> Result<Self, wasmer::ExportError> {
+//             let func_add_three = _instance
+//                 .exports
+//                 .get_function("add-three")
+//                 .expect("GET IT")
+//                 .typed(&store)
+//                 .unwrap_or_else(|e| panic!("{e:?}"));
+//             Ok(ProtocolPlugin {
+//                 func_add_three,
+//                 env,
+//             })
+//         }
+//         pub fn add_three(
+//             &self,
+//             store: &mut wasmer::Store,
+//             number: u32,
+//         ) -> Result<u32, wasmer::RuntimeError> {
+//             let result0 = self
+//                 .func_add_three
+//                 .call(store, wai_bindgen_wasmer::rt::as_i32(number))?;
+//             Ok(result0 as u32)
+//         }
+//     }
+//     #[allow(unused_imports)]
+//     use wasmer::AsStoreMut as _;
+//     #[allow(unused_imports)]
+//     use wasmer::AsStoreRef as _;
+// }
 
 // When the `wee_alloc` feature is enabled, use `wee_alloc` as the global
 // allocator.
@@ -115,6 +115,13 @@ pub fn return_eight() -> i32 {
     add_three(5) as _
 }
 
+#[wasm_bindgen]
+pub fn return_color() -> String {
+    console_error_panic_hook::set_once();
+    // alert(&format!("5 + 3 = {:?}", add_three(5)));
+    format!("{:?}", get_color())
+}
+
 fn add_three(number: u32) -> u32 {
     let (mut store, module) = create_store();
 
@@ -125,6 +132,16 @@ fn add_three(number: u32) -> u32 {
     plugin
         .add_three(&mut store, number)
         .expect("should add three")
+}
+
+fn get_color() -> protocol_plugin::Color {
+    let (mut store, module) = create_store();
+
+    let (plugin, _) =
+        protocol_plugin::ProtocolPlugin::instantiate(&mut store, &module, &mut imports! {})
+            .expect("should create instance");
+
+    plugin.get_color(&mut store).expect("should get color")
 }
 
 fn create_store() -> (Store, Module) {
