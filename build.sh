63ee7b616d1ac1c7e0c2624bc2f1b153a592c46f
kajacx <kajacx@gmail.com>
2023-05-24 20:55:19 +0200

    Update code to make it work

--- build.sh
+++ build.sh
@@ -2,6 +2,8 @@
 
 cd simple-plugin && cargo build --target wasm32-unknown-unknown && cd .. && \
 cd hello-wasmer-u64 && wasm-pack build --target web && \
-cd pkg && docker-compose -p wasmer-u64 down && docker-compose -p wasmer-u64 up -d && \
+cp pkg/hello_wasmer_u64_bg.wasm out/hello_wasmer_u64_bg.wasm && \
+cp pkg/hello_wasmer_u64.js out/hello_wasmer_u64.js && \
+cd out && docker-compose -p wasmer-u64 down && docker-compose -p wasmer-u64 up -d && \
 cd ../.. && \
 echo "All done, view the webpage at http://127.0.0.1:8094"
