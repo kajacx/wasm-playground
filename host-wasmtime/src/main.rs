c60421b19eb19a79e2241d5c49ca4ec0f4a28b7e
kajacx <kajacx@gmail.com>
2023-05-29 19:01:00 +0200

    Add arc mutex to wasmtime runtime

--- host-wasmtime/src/main.rs
+++ host-wasmtime/src/main.rs
@@ -1,3 +1,5 @@
+use std::sync::{Arc, Mutex};
+
 use bevy::prelude::Color;
 use bevy::prelude::*;
 use wai_bindgen_wasmtime::wasmtime::*;
@@ -6,8 +8,21 @@ const PLUGIN_BYTES: &'static [u8] =
     include_bytes!("../../plugin/target/wasm32-unknown-unknown/debug/bevy_plugin.wasm");
 
 wai_bindgen_wasmtime::import!("../protocol-plugin.wai");
+wai_bindgen_wasmtime::export!("../protocol-host.wai");
 
+use protocol_host::*;
 use protocol_plugin::*;
+
+struct MyData {
+    data: ProtocolPluginData,
+    red: Arc<Mutex<f32>>,
+}
+
+impl ProtocolHost for MyData {
+    fn get_red(&mut self) -> f32 {
+        *self.red.try_lock().expect("should lock mutex")
+    }
+}
 
 fn main() {
     let plugin_color = get_color();
@@ -28,14 +43,22 @@ fn get_color() -> protocol_plugin::Color {
     let config = Config::new();
     let engine = Engine::new(&config).expect("should create engine");
 
-    let mut linker = Linker::<ProtocolPluginData>::new(&engine);
+    let mut linker = Linker::<MyData>::new(&engine);
+    add_to_linker(&mut linker, |data| data).expect("should add to linker");
 
     let module = Module::new(&engine, PLUGIN_BYTES).expect("should load module from bytes");
 
-    let mut store = Store::new(&engine, ProtocolPluginData::default());
+    let red = Arc::new(Mutex::new(0.8f32));
+    let mut store = Store::new(
+        &engine,
+        MyData {
+            data: ProtocolPluginData::default(),
+            red: red.clone(),
+        },
+    );
 
     let (plugin, _instance) =
-        ProtocolPlugin::instantiate(&mut store, &module, &mut linker, |data| data)
+        ProtocolPlugin::instantiate(&mut store, &module, &mut linker, |data| &mut data.data)
             .expect("should create instance");
 
     plugin.get_color(&mut store).expect("should get color")
