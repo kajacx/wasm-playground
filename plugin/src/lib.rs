e649c8bccb856f406f0533ff09c77ba44d45bff3
kajacx <kajacx@gmail.com>
2023-06-21 19:30:52 +0200

    Imported fn example

--- plugin/src/lib.rs
+++ plugin/src/lib.rs
@@ -7,3 +7,13 @@ pub fn add_i32(a: i32, b: i32) -> i32 {
 pub fn add_sub_ten_i32(number: i32) -> (i32, i32) {
     (number.wrapping_add(10), number.wrapping_sub(10))
 }
+
+#[no_mangle]
+pub fn add_three_i32(number: i32) -> i32 {
+    unsafe { add_one_i32(number.wrapping_add(1)).wrapping_add(1) }
+}
+
+#[link(wasm_import_module = "imported_fns")]
+extern "C" {
+    fn add_one_i32(number: i32) -> i32;
+}
--- runtime/src/main.rs
+++ runtime/src/main.rs
@@ -10,7 +10,13 @@ fn main() -> Result<(), Box<dyn Error>> {
 
     let module = Module::new(&store.engine(), PLUGIN_BYTES)?;
 
-    let instance = Instance::new(&mut store, &module, &[])?;
+    let mut linker = Linker::new(store.engine());
+    linker.func_wrap("imported_fns", "add_one_i32", |_: Caller<()>, val: i32| {
+        val.wrapping_add(1)
+    })?;
+    let instance = linker.instantiate(&mut store, &module)?;
+
+    // let instance = Instance::new(&mut store, &module, &[])?;
 
     // Multiple arguments
     let add_i32 = instance.get_typed_func::<(i32, i32), i32>(&mut store, "add_i32")?;
@@ -26,6 +32,12 @@ fn main() -> Result<(), Box<dyn Error>> {
     println!("RES: {:?}", res);
     // assert_eq!(a, 50 + 10);
     // assert_eq!(b, 50 - 10);
+
+    // Call imported fn
+
+    let add_three_i32 = instance.get_typed_func::<i32, i32>(&mut store, "add_three_i32")?;
+    let returned = add_three_i32.call(&mut store, 5)?;
+    println!("IT IS: {}", returned);
 
     Ok(())
 }
