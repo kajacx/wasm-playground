134e9a55095834935404aa1ac9ce7b7b3bb5987a
kajacx <kajacx@gmail.com>
2023-06-22 10:22:29 +0200

    Import function in TS

--- ts-runtime/src/index.ts
+++ ts-runtime/src/index.ts
@@ -1,1 +1,15 @@
-console.log("Hello TS");
+import { readFileSync } from "node:fs";
+
+let bytes = readFileSync(
+  "../plugin/target/wasm32-unknown-unknown/debug/wasmtime_plugin.wasm"
+);
+
+WebAssembly.instantiate(bytes, {
+  imported_fns: {
+    add_one_i32(num: number) {
+      return num + 1;
+    },
+  },
+}).then((wasm) => {
+  console.log("Hello", wasm, (wasm as any).instance.exports.add_three_i32(5));
+});
