6be4c493abae59155da757c65c90e4d4b7bf82df
kajacx <kajacx@gmail.com>
2023-06-26 09:40:54 +0200

    Add pairs

--- plugin/src/lib.rs
+++ plugin/src/lib.rs
@@ -13,7 +13,21 @@ pub fn add_three_i32(number: i32) -> i32 {
     unsafe { add_one_i32(number.wrapping_add(1)).wrapping_add(1) }
 }
 
+#[no_mangle]
+pub fn add_three_pair(a: i32, b: f32) -> (i32, f32) {
+    let a = a.wrapping_add(1);
+    let b = b + 1.0;
+
+    let (a, b) = unsafe { add_one_pair(a, b) };
+
+    let a = a.wrapping_add(1);
+    let b = b + 1.0;
+
+    (a, b)
+}
+
 #[link(wasm_import_module = "imported_fns")]
 extern "C" {
     fn add_one_i32(number: i32) -> i32;
+    fn add_one_pair(a: i32, b: f32) -> (i32, f32);
 }
--- runtime/src/main.rs
+++ runtime/src/main.rs
@@ -14,6 +14,11 @@ fn main() -> Result<(), Box<dyn Error>> {
     linker.func_wrap("imported_fns", "add_one_i32", |_: Caller<()>, val: i32| {
         val.wrapping_add(1)
     })?;
+    linker.func_wrap(
+        "imported_fns",
+        "add_one_pair",
+        |_: Caller<()>, a: i32, b: f32| (a.wrapping_add(1), b + 1.0),
+    )?;
     let instance = linker.instantiate(&mut store, &module)?;
 
     // let instance = Instance::new(&mut store, &module, &[])?;
@@ -39,5 +44,21 @@ fn main() -> Result<(), Box<dyn Error>> {
     let returned = add_three_i32.call(&mut store, 5)?;
     println!("IT IS: {}", returned);
 
+    // Pair of numbers
+    let add_three_pair =
+        instance.get_typed_func::<(i32, f32), (i32, f32)>(&mut store, "add_three_pair")?;
+    let returned = add_three_pair.call(&mut store, (5, 15.0))?;
+    println!("Pair: {returned:?}");
+
+    why(&mut store, &instance);
+
     Ok(())
+}
+
+fn why(mut store: &mut Store<()>, instance: &Instance) {
+    let add_three_pair = instance
+        .get_typed_func::<(i32, f32), (i32, f32)>(&mut store, "add_three_pair")
+        .unwrap();
+    let returned = add_three_pair.call(&mut store, (5, 15.0)).unwrap();
+    println!("Pair: {returned:?}");
 }
