a3166a80b4dbbbd0aaafe0606d398d1e600ab013
kajacx <kajacx@gmail.com>
2023-07-01 10:39:12 +0200

    Fix return value

--- runtime-rust-web/src/lib.rs
+++ runtime-rust-web/src/lib.rs
@@ -2,9 +2,11 @@ use wasm_bindgen::prelude::*;
 
 #[wasm_bindgen]
 pub fn compute() -> String {
+    console_error_panic_hook::set_once();
     compute_it()
 }
 
 fn compute_it() -> String {
+    // panic!("WHYYYY");
     "ffooo".into()
 }
