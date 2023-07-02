0c8a931686a0fced09f91cf08c0d5f5254be0596
kajacx <kajacx@gmail.com>
2023-07-02 09:31:56 +0200

    Fix import error

--- plugin-rust/build-plugin.sh
+++ plugin-rust/build-plugin.sh
@@ -9,3 +9,7 @@ cargo build --target=wasm32-unknown-unknown
 cd target/wasm32-unknown-unknown/debug/
 wasm-tools component new plugin_rust.wasm -o component.wasm
 jco transpile component.wasm --instantiation -o out-dir
+
+# Fix import error
+sed -E -i 's/const print = imports.default;/const print = imports.print;/' out-dir/component.js
+sed -E -i 's/const importPoint = imports.default;/const importPoint = imports.importPoint;/' out-dir/component.js
