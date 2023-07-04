79d067d7c560e562bc7130648471b68293c9deb0
kajacx <kajacx@gmail.com>
2023-07-04 12:51:39 +0200

    Use wasmtime, sys works but not js

--- calculator/src/lib.rs
+++ calculator/src/lib.rs
@@ -1,7 +1,31 @@
+use wasmtime::*;
+
 pub fn calculate_plus_three(number: i32) -> String {
-    format!("{} + 3 = {}", number, add_three(number))
+    match add_three(number) {
+        Ok(result) => format!("{} + 3 = {}", number, result),
+        Err(err) => format!("{err:?}"),
+    }
 }
 
-fn add_three(number: i32) -> i32 {
-    number + 3
+fn add_three(number: i32) -> Result<i32> {
+    let wat = r#"
+      (module
+        (func $add_three (export "add_three")
+          (param $p0 i32) (result i32)
+          (i32.add (local.get $p0) (i32.const 3))
+        )
+      )
+    "#;
+
+    let mut store = Store::<()>::default();
+
+    let module = Module::new(&store.engine(), wat.as_bytes())?;
+
+    let instance = Instance::new(&mut store, &module, &[])?;
+
+    let add_three_wasm = instance.get_typed_func::<i32, i32>(&mut store, "add_three")?;
+
+    let result = add_three_wasm.call(&mut store, number)?;
+
+    Ok(result)
 }
