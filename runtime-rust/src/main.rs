677716811b84f760589d5576bec84db44902f04f
kajacx <kajacx@gmail.com>
2023-07-25 15:07:18 +0200

    Dynamically typed import fn

--- runtime-rust/src/main.rs
+++ runtime-rust/src/main.rs
@@ -16,28 +16,48 @@ fn main() -> Result<(), Box<dyn Error>> {
     Ok(())
 }
 
+#[allow(warnings)]
 fn run_wasm() -> Result<(), Box<dyn Error>> {
     let mut store = Store::<()>::default();
 
     let module = Module::new(&store.engine(), PLUGIN_BYTES)?;
 
     let mut linker = Linker::new(store.engine());
-    linker.func_wrap("imported_fns", "add_one_i32", |_: Caller<()>, val: i32| {
-        val.wrapping_add(1)
-    })?;
+    // linker.func_wrap("imported_fns", "add_one_i32", |_: Caller<()>, val: i32| {
+    //     val.wrapping_add(1)
+    // })?;
     linker.func_wrap(
         "imported_fns",
         "add_one_pair",
         |_: Caller<()>, a: i32, b: f32| (a.wrapping_add(1), b + 1.0),
     )?;
+
+    linker.func_new(
+        "imported_fns",
+        "add_one_i32",
+        FuncType::new([ValType::I32], [ValType::I32]),
+        |_caller, args, rets| match args[0] {
+            Val::I32(val) => {
+                rets[0] = Val::I32(val + 1);
+                Ok(())
+            }
+            _ => panic!(),
+        },
+    )?;
+
     let instance = linker.instantiate(&mut store, &module)?;
 
     // Call imported fn
-    let add_three_i32 = instance.get_typed_func::<i32, i32>(&mut store, "add_three_i32")?;
+    // let add_three_i32 = instance.get_typed_func::<i32, i32>(&mut store, "add_three_i32")?;
 
-    let returned = add_three_i32.call(&mut store, 5)?;
-    println!("Add three: {returned:?}");
-    assert_eq!(returned, 5 + 3);
+    // let returned = add_three_i32.call(&mut store, 5)?;
+    let add_three_i32 = instance
+        .get_func(&mut store, "add_three_i32")
+        .expect("get add_three_i32");
+    let mut results = [Val::I32(0)];
+    add_three_i32.call(&mut store, &[Val::I32(5)], &mut results)?;
+    println!("Add three: {results:?}");
+    // assert_eq!(returned, 5 + 3);
 
     // Multiple arguments
     let add_i32 = instance.get_typed_func::<(i32, i32), i32>(&mut store, "add_i32")?;
