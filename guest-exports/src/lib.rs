623b6bf44562cb799e8ab86eaa1a24b51a784a6f
kajacx <kajacx@gmail.com>
2023-12-23 13:07:04 +0100

    Add some import testing

--- guest-exports/src/lib.rs
+++ guest-exports/src/lib.rs
@@ -9,82 +9,25 @@ wit_bindgen::generate!({
 struct Component;
 
 impl Guest for Component {
-    fn xy_export(_d: Xy) -> Xy {
-        todo!()
+    fn export_vector_round(v: Vector) -> Vector {
+        import_vector_round(v)
     }
 
-    fn xy_export_tuple(_d: (Xy, Xy)) -> (Xy, Xy) {
-        todo!()
+    fn export_vector_list(l: Vec<Vector>) -> Vec<Vector> {
+        import_vector_list(&l)
     }
 
-    fn xy_export_pair(_d: Xy, _e: Xy) -> (Xy, Xy) {
-        todo!()
+    fn export_s32(s: i32) -> i32 {
+        import_s32(s)
     }
 
-    fn xy_export_list(_d: Vec<Xy>) -> Vec<Xy> {
-        todo!()
+    fn export_string(s: String) -> String {
+        import_string(&s)
     }
 
-    fn xyz_export(_d: Xyz) -> Xyz {
-        todo!()
-    }
-
-    fn xyz_export_tuple(_d: (Xyz, Xyz)) -> (Xyz, Xyz) {
-        todo!()
-    }
-
-    fn xyz_export_pair(_d: Xyz, _e: Xyz) -> (Xyz, Xyz) {
-        todo!()
-    }
-
-    fn xyz_export_list(_d: Vec<Xyz>) -> Vec<Xyz> {
-        todo!()
-    }
-
-    fn void_args_s32() -> i32 {
-        todo!()
-    }
-
-    fn void_ret_s32(_d: i32) {
-        todo!()
-    }
-
-    fn void_both() {
-        todo!()
-    }
-
-    fn points(_d: Vec<Point16>) -> Vec<Point16> {
-        todo!()
-    }
-
-    fn points_more(_d: Vec<PointMore>) -> Vec<PointMore> {
-        todo!()
-    }
-
-    fn points_final(_d: Vec<PointFinal>) -> Vec<PointFinal> {
-        todo!()
-    }
-
-    fn player_look_at(_p: Player, _l: Player) -> Player {
-        todo!()
-    }
-
-    fn char_round(_c: char) -> char {
-        todo!()
-    }
-
-    fn char_list(_c: Vec<char>) -> Vec<char> {
-        todo!()
-    }
-
-    fn bool_round(_c: bool) -> bool {
-        todo!()
-    }
-
-    fn bool_list(_c: Vec<bool>) -> Vec<bool> {
-        let _ = import_s32(5);
-        import_vector_arg(Vector { x: 1, y: 2, z: 3 });
-        let _ = import_vector_res();
-        todo!()
+    fn export_insane(
+        i: (Vector, Vector, Vector, Vector, Vector, Vector),
+    ) -> (Vector, Vector, Vector, Vector, Vector, Vector) {
+        import_insane(i)
     }
 }
