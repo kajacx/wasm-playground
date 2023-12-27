f09fc4cb6c00cb4585a8f85f4f589749a23efd45
kajacx <kajacx@gmail.com>
2023-12-27 13:57:08 +0100

    Interfaces test

--- guest-exports/src/lib.rs
+++ guest-exports/src/lib.rs
@@ -2,18 +2,34 @@ wit_bindgen::generate!({
     path: "../exports.wit",
     world: "example",
     exports: {
-        world: Component,
+        // world: Component,
+        "component:guest-reactor/guest-add": ComponentAdd,
+        "guest-sub": ComponentSub,
     }
 });
 
-struct Component;
+// struct Component;
 
-impl Guest for Component {
-    fn export_single(e: Matrix, a: u32) -> Matrix {
-        import_single(&e, a)
+// impl Guest for Component {}
+
+struct ComponentAdd;
+
+impl exports::component::guest_reactor::guest_add::Guest for ComponentAdd {
+    fn add_three(num: i32) -> i32 {
+        let num = component::guest_reactor::host_add::add_one(num);
+        let num = component::guest_reactor::host_add::add_one(num);
+        let num = component::guest_reactor::host_add::add_one(num);
+        num
     }
+}
 
-    fn export_single_list(e: Vec<Matrix>) -> Vec<Matrix> {
-        import_single_list(&e)
+struct ComponentSub;
+
+impl exports::guest_sub::Guest for ComponentSub {
+    fn sub_three(num: i32) -> i32 {
+        let num = host_sub::sub_one(num);
+        let num = host_sub::sub_one(num);
+        let num = host_sub::sub_one(num);
+        num
     }
 }
