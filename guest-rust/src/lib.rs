d721d4298c366fe86f504e824cfec92da7c90b3d
kajacx <kajacx@gmail.com>
2023-12-31 13:17:18 +0100

    New resource example

--- guest-rust/src/lib.rs
+++ guest-rust/src/lib.rs
@@ -1,13 +1,24 @@
-use example::protocol::companies;
-
 wit_bindgen::generate!({
     path: "../protocol.wit",
     world: "my-world",
     exports: {
-        world: MyGuest,
+        "example:protocol/employees": MyEmployees,
         "example:protocol/employees/employee": MyEmployee,
     }
 });
+
+struct MyEmployees;
+
+impl exports::example::protocol::employees::Guest for MyEmployees {
+    fn find_job(
+        employee: &MyEmployee,
+        companies: wit_bindgen::rt::vec::Vec<exports::example::protocol::employees::Company>,
+    ) -> Option<exports::example::protocol::employees::Company> {
+        companies
+            .into_iter()
+            .find(|company| employee.min_salary <= company.get_max_salary())
+    }
+}
 
 pub struct MyEmployee {
     name: String,
@@ -25,18 +36,5 @@ impl exports::example::protocol::employees::GuestEmployee for MyEmployee {
 
     fn get_min_salary(&self) -> u32 {
         self.min_salary
-    }
-}
-
-struct MyGuest;
-
-impl Guest for MyGuest {
-    fn find_job(
-        employee: &Employee,
-        companies: Vec<companies::Company>,
-    ) -> Option<companies::Company> {
-        companies
-            .into_iter()
-            .find(|company| can_employee_work_at(employee, &company))
     }
 }
--- runtime-rust-wasmtime/src/main.rs
+++ runtime-rust-wasmtime/src/main.rs
@@ -1,5 +1,5 @@
-use example::protocol::{companies, employees};
-use std::{collections::HashMap, sync::Arc};
+use example::protocol::companies;
+use std::collections::HashMap;
 use wasmtime::{
     component::{Component, Linker, Resource},
     Config, Engine, Result, Store,
@@ -14,12 +14,6 @@ wasmtime::component::bindgen!({
 struct MyCompany {
     name: String,
     max_salary: u32,
-}
-
-#[derive(Default, Clone)]
-struct MyEmployee {
-    name: String,
-    min_salary: u32,
 }
 
 #[derive(Default, Clone)]
@@ -48,20 +42,6 @@ impl<T> ResHolder<T> {
 #[derive(Default, Clone)]
 struct State {
     companies: ResHolder<MyCompany>,
-    employees: ResHolder<MyEmployee>,
-    world: Option<Arc<MyWorld>>,
-}
-
-impl MyWorldImports for State {
-    fn can_employee_work_at(
-        &mut self,
-        employee: Resource<Employee>,
-        company: Resource<Company>,
-    ) -> Result<bool> {
-        let company = self.companies.get(company.rep()).unwrap();
-        let employee = self.employees.get(employee.rep()).unwrap();
-        Ok(employee.min_salary <= company.max_salary)
-    }
 }
 
 impl companies::HostCompany for State {
@@ -87,32 +67,6 @@ impl companies::HostCompany for State {
 
 impl companies::Host for State {}
 
-impl employees::HostEmployee for State {
-    fn new(&mut self, name: String, min_salary: u32) -> Result<Resource<employees::Employee>> {
-        Ok(Resource::new_own(
-            self.employees.new(MyEmployee { name, min_salary }),
-        ))
-    }
-
-    fn get_name(&mut self, self_: Resource<employees::Employee>) -> Result<String> {
-        Ok(self.employees.get(self_.rep()).unwrap().name.clone())
-    }
-
-    fn get_min_salary(
-        &mut self,
-        self_: wasmtime::component::Resource<employees::Employee>,
-    ) -> Result<u32> {
-        Ok(self.employees.get(self_.rep()).unwrap().min_salary)
-    }
-
-    fn drop(&mut self, rep: wasmtime::component::Resource<employees::Employee>) -> Result<()> {
-        self.employees.drop(rep.rep()).unwrap();
-        Ok(())
-    }
-}
-
-impl employees::Host for State {}
-
 fn main() {
     let mut config = Config::new();
     config.wasm_component_model(true);
@@ -130,23 +84,15 @@ fn main() {
 
     MyWorld::add_to_linker(&mut linker, |state| state).unwrap();
     let (my_world, _instance) = MyWorld::instantiate(&mut store, &component, &linker).unwrap();
-    let my_world = Arc::new(my_world);
-    store.data_mut().world = Some(my_world.clone());
 
     let employees = my_world.example_protocol_employees().employee();
     let employee = employees
         .call_constructor(&mut store, "Mike".into(), 50_000)
         .unwrap();
-    println!("What can this be used for? {employee:?}");
     println!(
         "Employee name: {}",
         employees.call_get_name(&mut store, employee).unwrap()
     );
-
-    let employee = Resource::new_own(store.data_mut().employees.new(MyEmployee {
-        name: "Mike".into(),
-        min_salary: 50_000,
-    }));
 
     let company1 = Resource::new_own(store.data_mut().companies.new(MyCompany {
         name: "Company1".into(),
@@ -154,11 +100,8 @@ fn main() {
     }));
 
     let result = my_world
-        .call_find_job(
-            &mut store,
-            Resource::new_borrow(employee.rep()),
-            &[company1],
-        )
+        .example_protocol_employees()
+        .call_find_job(&mut store, employee, &[company1])
         .unwrap();
     println!("Find first job: {result:?}");
 
@@ -172,6 +115,7 @@ fn main() {
     }));
 
     let result = my_world
+        .example_protocol_employees()
         .call_find_job(&mut store, employee, &[company1, company2])
         .unwrap();
     println!("Find second job: {result:?}");
