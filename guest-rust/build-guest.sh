88748289344a5c2e1b4ebd82ee2b99e7a56be6cd
kajacx <kajacx@gmail.com>
2024-04-24 10:56:12 +0200

    Update to wasmtime 20

--- guest-rust/build-guest.sh
+++ guest-rust/build-guest.sh
@@ -4,8 +4,8 @@ set -e
 # Run from the parten directory
 
 cd guest-rust
-# cargo component build --release --target wasm32-unknown-unknown
-cargo build --release --target wasm32-unknown-unknown
+cargo component build --release --target wasm32-unknown-unknown
+# cargo build --release --target wasm32-unknown-unknown
 
 cd target/wasm32-unknown-unknown/release/
 # jco transpile guest_rust.wasm --instantiation -o out-dir
